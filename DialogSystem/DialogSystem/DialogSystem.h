eca762013d7b4f7e265c403b13b5cd048b02fc94
RoperoIvan <36190271+RoperoIvan@users.noreply.github.com>
2019-03-27 11:28:45 +0100

    Implementing the code to a base code

---
+++ DialogSystem/DialogSystem/DialogSystem.h
@@ -0,0 +1,64 @@
+#ifndef _DIALOGSYSTEM_H
+#define _DIALOGSYSTEM_H
+
+#include <string>
+#include <vector>
+#include "j1App.h"
+#include "p2Log.h"
+
+using namespace std;
+
+struct DialogNode;
+
+struct DialogueOption
+{
+public:
+	string text;
+	DialogNode* nextNode;
+	int id;
+	int returnCode;
+};
+
+struct DialogNode
+{
+public:
+	string text;
+	vector <DialogueOption> dialogOptions;
+	int id;
+};
+class DialogTree
+{
+public:
+	DialogTree() {};
+	~DialogTree() {};
+
+
+public:
+	vector <DialogNode*> dialogNodes;
+	int treeid, karma;
+	bool CheckTags(int tag);
+};
+
+class Dialogue
+{
+public:
+	Dialogue();
+
+	void init();
+	void destroytree();
+
+	int performdialogue(int treeid);
+	bool LoadDialogue(const char*);
+	bool LoadTreeData(pugi::xml_node& trees, DialogTree* oak);
+	bool LoadNodesDetails(pugi::xml_node& text_node, DialogNode* npc);
+	
+private:
+	vector <DialogTree*> dialogTrees;
+
+public:
+	pugi::xml_document	tree_file;
+	pugi::xml_node		tree;
+
+};
+
+#endif
---
+++ DialogSystem/DialogSystem/j1UIManager.h
@@ -0,0 +1,64 @@
+#ifndef __j1UIMANAGER_H__
+#define __j1UIMANAGER_H__
+
+#include "j1Module.h"
+#include "p2Animation.h"
+#include <list>
+
+enum UIType
+{
+	IMAGE,
+	LABEL,
+	BUTTON,
+	NON,
+};
+
+struct SDL_Texture;
+struct SDL_Rect;
+
+enum Color;
+
+class GUI;
+class GUI_Image;
+class GUI_Label;
+class GUI_Button;
+
+class j1UIManager: public j1Module
+{
+public:
+	j1UIManager();
+	virtual ~j1UIManager();
+
+	bool Awake(pugi::xml_node&);
+	bool Start();
+	bool PreUpdate();
+	bool PostUpdate();
+	bool CleanUp();
+
+	GUI_Image* AddImage(const int &x, const int &y, const SDL_Rect & rect, j1Module * callback, GUI * parent, bool draw, bool drag, bool interact);
+	GUI_Button* AddButton(const int &x, const int &y, const SDL_Rect &idle, const SDL_Rect &mouse_in, const SDL_Rect &clicked, j1Module* callback, GUI* parent, bool draw, bool drag, bool inter);
+	GUI_Label* AddLabel(const int &x, const int &y, const char* text, uint size, GUI* parent, Color color, const char* font, j1Module* callback);
+
+	void CreateScreen();
+
+	bool DeleteUIElement(GUI * element);
+	void BFS(std::list<GUI *> &visited, GUI * elem);
+	bool DeleteAllUIElements();
+	bool GetElemOnMouse(int x, int y, GUI* & element);
+	bool CheckCollision(int x, int y, GUI* item);
+	void UI_Events(GUI* element);
+
+	const SDL_Texture* GetAtlas() const;
+
+private:
+
+	SDL_Texture* atlas = nullptr;
+	std::list<GUI*> ui_list;	
+
+public:
+
+	GUI * screen = nullptr;
+	bool debug_ui = false;
+
+};
+#endif // !__j1UIMANAGER_H__
